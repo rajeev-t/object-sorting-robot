d15570c6b6245dcb93df43212640a609beeb6ebb
rajeev-t <thimmareddyrajeev@gmail.com>
2023-04-30 19:10:10 -0700

    added specific angles to use for servos

--- servo_base.py
+++ servo_base.py
@@ -4,11 +4,9 @@ import time
 
 servo = PWM(Pin(15))
 servo.freq(100)
-duty_min = int((1/20)*65535)
-duty_max = int((3/20)*65535)
 
-while True:
-    servo.duty_u16(duty_min)
-    time.sleep(2)
-    servo.duty_u16(duty_max)
-    time.sleep(2)
+duty_unsorted = int((4/80)*65535)
+duty_90_from_unsorted = int((12/80)*65535)
+duty_test = int((12/80)*65535)
+
+servo.duty_u16(duty_test)
--- servo_joints.py
+++ servo_joints.py
@@ -4,12 +4,15 @@ import time
 
 servo = PWM(Pin(15))
 servo.freq(50)
-duty_180 = int((1/20)*65535)
-duty_135 = int((1/15)*65535)
-duty_90 = int((1/10)*65535)
-duty_45 = int((1/8)*65535)
 
-servo.duty_u16(duty_45)
+# move to standby, place links at a 90 angle
+duty_standby = int((1/10)*65535)
+duty_pickup = int((1/15)*65535)
+
+duty_180 = int((1/20)*65535)
+# duty_45 = int((1/8)*65535)
+
+servo.duty_u16(duty_pickup)
 
 #while True:
     #servo.duty_u16(duty_min)
